83c0c9b4e6d8104d86a381c5a618e8de19299418
Skyuzii <Skyuzii@yandex.ru>
2021-07-31 13:42:54 +0300

    Init

---
+++ PrayWay.Api/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrayWay.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]/[action]")]
+    public class BaseController : ControllerBase
+    {
+    }
+}
---
+++ PrayWay.Application/Configure.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PrayWay.Application
+{
+    public static class Configure
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            services.AddMediatR(assembly);
+            services.AddAutoMapper(assembly);
+            services.AddValidatorsFromAssembly(assembly);
+
+            return services;
+        }
+    }
+}
